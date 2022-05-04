0de119e6a06299850bbf8b436d73cb4511cb6b5e
ChanDara Leav <chandara@nokor-tech.com>
2022-05-04 21:04:16 +0700

    build: automata marchine

--- src/lib/automaton.ts
+++ src/lib/automaton.ts
@@ -1,4 +1,5 @@
 import { Automaton, AutomatonDefinition } from "./automaton.interface";
+import { omit } from 'lodash';
 
 export function create(defition: AutomatonDefinition): Automaton {
   return Object.freeze(defition);
@@ -27,7 +28,29 @@ export function isNonDeterministic(automaton: Automaton): boolean {
 }
 
 export function minimize(automaton: Automaton): Automaton {
-  throw new Error("TODO");
+  let startState = automaton.startState;
+  let minimizeAutomaton = automaton;
+
+  //Step 1
+  //remove non accesible
+  let wantedState = Array(startState);
+  let states = Array();
+  Object.keys(automaton.states).forEach(state => {
+    if (startState == state || !wantedState.includes(state)) {
+      Object.keys(automaton.states[state]['on']).forEach(symbol => {
+        if (!wantedState.includes(automaton.states[state]['on'][symbol])) {
+          wantedState.push(automaton.states[state]['on'][symbol])
+        }
+      });
+    }
+    states.push(state);
+  });
+  Object.keys(minimizeAutomaton.states).forEach(key => {
+    if (!wantedState.includes(key)) delete minimizeAutomaton.states[key]
+  });
+
+  //Step 2
+
 }
 
 export function determinize(automaton: Automaton): Automaton {
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import PrimeVue from "primevue/config";
 import "primevue/resources/themes/lara-light-purple/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
 
 const app = createApp(App);
 app.use(PrimeVue);
-
 app.mount("#app");
