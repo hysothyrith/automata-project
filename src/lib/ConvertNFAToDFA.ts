72cf8a4e78c7e677387f19f5cfef97ede5e1d910
ChanDara Leav <chandara@nokor-tech.com>
2022-05-08 13:17:47 +0700

    add dist

--- src/lib/ConvertNFAToDFA.ts
+++ src/lib/ConvertNFAToDFA.ts
@@ -2,7 +2,5 @@ import { Automaton} from "./automaton.interface";
 
 export const ConvertNFAToDFA =(automaton:Automaton):any=>{
     const automaton1 = automaton 
-    // δ'([q0], 0) = [q0]  
-    // δ'([q0], 1) = [q1] 
     return "ConvertNFAToDFA"
 }
