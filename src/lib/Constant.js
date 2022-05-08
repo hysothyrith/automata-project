9966d69f53f7923720332cabb6f3efc5ab1a082c
ChanDara Leav <chandara@nokor-tech.com>
2022-05-08 15:52:40 +0700

    add export const SMALL_PHI = "φ"

--- src/lib/Constant.js
+++ src/lib/Constant.js
@@ -1,5 +1,5 @@
 export const EPSILON = "ε";
-
+export const SMALL_PHI = "φ"
 
 export const NFA1ForConvert= {
   symbols: ["0", "1"],
@@ -7,6 +7,7 @@ export const NFA1ForConvert= {
     q0: { on: { 0: "q0", 1: "q1" } },
     q1: { on: { 0: ["q1", "q2"], 1: "q1" } },
     q2: { on: { 0: "q2", 1: ["q1", "q2"] } },
+    q2: { on: { 0: "", 1: ["q1", "q2"] } },
   },
   startState: "q0",
   finalStates: ["q2"],
